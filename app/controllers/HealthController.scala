7b8982f294c9ba874b47c2efe02280bdc1970a38
Jarl André Hübenthal <jarlah@users.noreply.github.com>
2019-05-04 22:36:43 +0200

    implementing file controller + fixing health controller

--- app/controllers/HealthController.scala
+++ app/controllers/HealthController.scala
@@ -1,5 +1,16 @@
 package controllers
 
-class HealthController {
+import dbi.DBIHolder
+import javax.inject.{Inject, Singleton}
+import play.api.mvc.{AbstractController, Action, AnyContent, ControllerComponents}
 
+@Singleton
+class HealthController @Inject()(cc: ControllerComponents, dbiHolder: DBIHolder) extends AbstractController(cc) {
+
+  def ok: Action[AnyContent] = Action {
+    Option(dbiHolder.dbi.getDbiThrowable)
+        .map(throwable => InternalServerError("ERROR: DBI reported error:\n" + throwable + "\n" +
+          throwable.getStackTrace.map(_.toString).mkString("\n")))
+        .getOrElse(Ok("FREEACSOK"))
+  }
 }
