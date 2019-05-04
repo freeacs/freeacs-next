9340916460aadff08fbb1b531e006d5c0c256913
Jarl André Hübenthal <jarlah@users.noreply.github.com>
2019-05-04 21:55:04 +0200

    display unit count in dashboard

--- app/controllers/WebController.scala
+++ app/controllers/WebController.scala
@@ -1,23 +1,17 @@
 package controllers
 
+import scala.collection.JavaConverters._
+import dbi.DBIHolder
 import javax.inject._
 import play.api.mvc._
 
-/**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
 @Singleton
-class WebController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class WebController @Inject()(cc: ControllerComponents, dbiHolder: DBIHolder) extends AbstractController(cc) {
 
-  /**
-   * Create an Action to render an HTML page with a welcome message.
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
-  def index = Action {
-    Ok(views.html.dashboard())
+  def index: Action[AnyContent] = Action {
+    val dbi = dbiHolder.dbi
+    val count = dbi.getACSUnit.getUnitCount(dbi.getAcs.getUnittypes.getUnittypes.toList.asJava)
+    Ok(views.html.dashboard(count))
   }
 
 }
