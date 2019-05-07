271c2f40aedf559bb3fd0baaeed79471bfc0efc1
Jarl André Hübenthal <jarlah@users.noreply.github.com>
2019-05-07 19:46:05 +0200

    adding a simple controller to see that I can get soap and parse it

--- app/AppLoader.scala
+++ app/AppLoader.scala
@@ -35,13 +35,14 @@ class AppComponents(context: Context)
   lazy val profileService      = new ProfileService(dbConf)
   lazy val unitService         = new UnitService(dbConf)
   lazy val unitController      = new UnitController(cc, unitService, profileService, unitTypeService)
+  lazy val tr069Controller     = new Tr069Controller(cc, unitService, profileService, unitTypeService)
   lazy val unitTypeController  = new UnitTypeController(cc, unitTypeService)
   lazy val profileController   = new ProfileController(cc, profileService, unitTypeService)
   lazy val dashboardController = new DashboardController(cc, unitService)
   lazy val healthController    = new HealthController(cc)
   lazy val properties          = new Properties(config)
   lazy val dbiHolder           = new DBIHolder(config, dbApi.database("default"))
-  lazy val tr069Controller     = new Tr069Controller(cc, properties, baseCache, config, dbiHolder, unitService)
+  lazy val oldTr069Ctrl        = new OldTr069Controller(cc, properties, baseCache, config, dbiHolder, unitService)
 
   override val router: Router = Router.from {
     case GET(p"/")                  => dashboardController.index
@@ -55,8 +56,9 @@ class AppComponents(context: Context)
     case GET(p"/unit/create")       => unitController.viewCreate
     case POST(p"/unit/create")      => unitController.postCreate
     case GET(p"/unit/overview")     => unitController.overview
-    case POST(p"/tr069")            => tr069Controller.provision
-    case POST(p"/tr069/prov")       => tr069Controller.provision
+    case POST(p"/tr069")            => oldTr069Ctrl.provision
+    case POST(p"/tr069/prov")       => oldTr069Ctrl.provision
+    case POST(p"/test")             => tr069Controller.provision
     case GET(p"/assets/$file*")     => assets.versioned("/public", file)
   }
 }
--- app/controllers/Tr069Controller.scala
+++ app/controllers/Tr069Controller.scala
@@ -1,86 +1,28 @@
 package controllers
-
-import com.github.jarlah.authenticscala.Authenticator._
-import com.github.jarlah.authenticscala.{AuthenticationContext, Authenticator}
-import com.typesafe.config.Config
-import freeacs.dbi.DBIHolder
-import freeacs.tr069.Properties
-import freeacs.tr069.base.BaseCache
-import freeacs.tr069.http.HTTPRequestResponseData
-import freeacs.tr069.methods.ProvisioningStrategy
+import models.Method
 import play.api.Logging
-import play.api.mvc._
-import services.UnitService
+import play.api.i18n.I18nSupport
+import play.api.mvc.{AbstractController, ControllerComponents, Request}
+import services.{ProfileService, UnitService, UnitTypeService}
 
 import scala.concurrent.{ExecutionContext, Future}
 
 class Tr069Controller(
     cc: ControllerComponents,
-    properties: Properties,
-    baseCache: BaseCache,
-    config: Config,
-    dbiHolder: DBIHolder,
-    unitDetails: UnitService
-)(implicit ec: ExecutionContext)
-    extends AbstractController(cc)
+    unitService: UnitService,
+    profileService: ProfileService,
+    unitTypeService: UnitTypeService
+)(
+    implicit ec: ExecutionContext
+) extends AbstractController(cc)
+    with I18nSupport
     with Logging {
 
-  def provision = Action.async { req =>
-    config.getString("auth.method").toLowerCase match {
-      case method if method == "digest" && config.getString("digest.secret") == "changeme" =>
-        logger.error("Digest secret must be changed from its default value!")
-        Future.successful(InternalServerError(""))
-      case method if "none" != method =>
-        val context = AuthenticationContext(
-          req.method,
-          req.uri,
-          req.headers.toSimpleMap,
-          req.remoteAddress
-        )
-        for {
-          result <- Authenticator.authenticate(
-                     context,
-                     (unitId: String) => unitDetails.getSecret(unitId).map(_.orNull),
-                     method
-                   )
-        } yield {
-          if (result.success) {
-            processRequest(req, result.principal)
-          } else {
-            Unauthorized.withHeaders(challenge(context, method).toSeq: _*)
-          }
-        }
-      case _ => Future.successful(processRequest(req))
-    }
-  }
-
-  private def processRequest(
-      req: Request[AnyContent],
-      principal: Option[String] = None
-  ): Result = {
-    val session = getSession(req)
-    val realIp  = req.headers.get("X-Real-IP").orNull
-    val reqRes = new HTTPRequestResponseData(
-      baseCache,
-      req.remoteAddress,
-      realIp,
-      session("uuid")
-    )
-    reqRes.getRequestData.setContextPath(config.getString("context-path"))
-    reqRes.getRequestData.setXml(req.body.asXml.map(_.toString).getOrElse(""))
-    if (principal.isDefined && reqRes.getSessionData.getUnit == null) {
-      val username    = principal.get
-      val sessionData = reqRes.getSessionData
-      sessionData.setUnitId(username)
-      sessionData.setUnit(dbiHolder.dbi.getACSUnit.getUnitById(username))
-    }
-    ProvisioningStrategy.getStrategy(properties, dbiHolder.dbi, baseCache).process(reqRes)
-    Ok(reqRes.getResponseData.getXml).withHeaders("SOAPAction" -> "").withSession(session)
-  }
-
-  private def getSession(req: Request[AnyContent]): Session = {
-    req.session.get("uuid").map(_ => req.session).getOrElse {
-      req.session + ("uuid" -> java.util.UUID.randomUUID.toString)
-    }
+  def provision = Action.async { implicit request =>
+    request.body.asXml
+      .flatMap(_.headOption)
+      .flatMap(Method.fromNode)
+      .map(method => Future.successful(Ok(method.abbr)))
+      .getOrElse(Future.successful(Ok))
   }
 }
