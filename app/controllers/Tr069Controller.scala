5e3e43f77888d135c5cca905f21d9602861d3f12
Jarl André Hübenthal <jarlah@users.noreply.github.com>
2019-05-08 21:31:47 +0200

    prepare for decision logic, return modified session data and response

--- app/controllers/Tr069Controller.scala
+++ app/controllers/Tr069Controller.scala
@@ -3,8 +3,9 @@ import models._
 import play.api.Logging
 import play.api.cache.AsyncCacheApi
 import play.api.i18n.I18nSupport
-import play.api.mvc.{AbstractController, ControllerComponents}
+import play.api.mvc.{AbstractController, ControllerComponents, Result}
 import services.{ProfileService, UnitService, UnitTypeService}
+
 import scala.concurrent.{ExecutionContext, Future}
 import scala.xml.Node
 
@@ -31,27 +32,36 @@ class Tr069Controller(
       }
       sessionId <- session.get("uuid")
     } yield {
-      getSessionData(sessionId, header, deviceId, payload).map { sessionData =>
-        method match {
-          case CwmpMethod.IN =>
-            val unitId = sessionData.unit.map(_.getId).getOrElse("N/A")
-            logger.warn(s"Got the following Inform from unit: $unitId:\n$sessionData")
-            Ok(
-              <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/">
-                <soapenv:Body>
-                  <cwmp:InformResponse xmlns:cwmp="urn:dslforum-org:cwmp-1-0">
-                    <MaxEnvelopes>1</MaxEnvelopes>
-                  </cwmp:InformResponse>
-                </soapenv:Body>
-              </soapenv:Envelope>
-            ).withHeaders("SOAPAction" -> "").withSession(session)
-          case _ =>
-            NotImplemented
-        }
-      }
+      getSessionData(sessionId, header, deviceId, payload).flatMap { sessionData =>
+        val result = processRequest(method, sessionData)
+        if (sessionData != result._1)
+          cache.set(s"$sessionId-data", result._1).map(_ => result._2)
+        else
+          Future.successful(result._2)
+      }.map(_.withSession(session))
     }).getOrElse {
       logger.warn("Got no payload and no method, assuming empty")
       Future.successful(Ok)
+    }
+  }
+
+  private def processRequest(method: CwmpMethod, sessionData: SessionData): (SessionData, Result) = {
+    method match {
+      case CwmpMethod.IN =>
+        val unitId = sessionData.unit.map(_.getId).getOrElse("N/A")
+        logger.warn(s"Got the following Inform from unit: $unitId:\n$sessionData")
+        val response = Ok(
+          <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/">
+            <soapenv:Body>
+              <cwmp:InformResponse xmlns:cwmp="urn:dslforum-org:cwmp-1-0">
+                <MaxEnvelopes>1</MaxEnvelopes>
+              </cwmp:InformResponse>
+            </soapenv:Body>
+          </soapenv:Envelope>
+        ).withHeaders("SOAPAction" -> "")
+        (sessionData, response)
+      case _ =>
+        (sessionData, NotImplemented)
     }
   }
 
