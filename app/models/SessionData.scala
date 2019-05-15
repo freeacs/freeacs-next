74e707c23d6f0c1f30fcb9a15a6ecebda3f83b73
Jarl André Hübenthal <jarlah@users.noreply.github.com>
2019-05-15 15:36:47 +0200

    adding cpe parameter names to session data

--- app/models/SessionData.scala
+++ app/models/SessionData.scala
@@ -12,17 +12,32 @@ case class SessionData(
     requests: Seq[CwmpMethod] = Seq.empty,
     cwmpVersion: String = "1-0"
 ) {
-  lazy val keyRoot: Option[String] = params
-    .map(_.name.split('.'))
-    .find(p => p(0) == "Device" || p(0) == "InternetGatewayDevice")
-    .map(_(0) + '.')
-  lazy val unitId: Option[String]       = username.orElse(unit.map(_.unitId).orElse(deviceId.map(_.unitId)))
-  lazy val serialNumber: Option[String] = deviceId.map(_.serialNumber.underlying)
-  lazy val firstConnect: Boolean        = unit.exists(!_.params.exists(_.unitTypeParamName != SECRET.name))
-  def unsafeGetUnitId: String           = unitId.get
+  import SessionData._
+  lazy val keyRoot                  = getKeyRoot(params)
+  lazy val CONFIG_FILES             = keyRoot + "DeviceInfo.VendorConfigFile."
+  lazy val SOFTWARE_VERSION         = keyRoot + "DeviceInfo.SoftwareVersion"
+  lazy val PERIODIC_INFORM_INTERVAL = keyRoot + "ManagementServer.PeriodicInformInterval"
+  lazy val CONNECTION_URL           = keyRoot + "ManagementServer.ConnectionRequestURL"
+  lazy val CONNECTION_PASSWORD      = keyRoot + "ManagementServer.ConnectionRequestPassword"
+  lazy val CONNECTION_USERNAME      = keyRoot + "ManagementServer.ConnectionRequestUsername"
+  lazy val unitId                   = username.orElse(unit.map(_.unitId).orElse(deviceId.map(_.unitId)))
+  lazy val serialNumber             = deviceId.map(_.serialNumber.underlying)
+  lazy val firstConnect             = unit.exists(!_.params.exists(_.unitTypeParamName != SECRET.name))
+  lazy val unsafeGetUnitId          = unitId.get
   def unsafeGetProductClass(appendHwVersion: Boolean): String =
+    SessionData.unsafeGetProductClass(deviceId, appendHwVersion)
+}
+
+object SessionData {
+  def unsafeGetProductClass(deviceId: Option[DeviceIdStruct], appendHwVersion: Boolean): String =
     deviceId.map { deviceId =>
       // TODO
       deviceId.productClass.underlying
     }.get
+
+  def getKeyRoot(params: Seq[ParameterValueStruct]) =
+    params
+      .map(_.name.split('.'))
+      .find(p => p(0) == "Device" || p(0) == "InternetGatewayDevice")
+      .map(_(0) + '.')
 }
