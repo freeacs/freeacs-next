5aa7c749634a6b7d86ec22b614025db3f752b343
Jarl André Hübenthal <jarlah@users.noreply.github.com>
2019-05-23 08:00:45 +0200

    get active menu links working with details pages + removing fontawesome

--- app/views/Page.scala
+++ app/views/Page.scala
@@ -2,21 +2,21 @@ package views
 
 sealed abstract class Page(
     val name: String,
-    val fontAwesome: String,
-    val url: String
+    val url: String,
+    val link: Boolean = true
 )
 
-case object Dashboard        extends Page("Dashboard", "fa fa-dashboard", "/")
-case object UnitTypeOverview extends Page("Unit Type Overview", "fa fa-desktop", "/unittype/overview")
-case object UnitTypeDetails  extends Page("Unit Type Details", "fa fa-desktop", "/unittype/details")
-case object CreateUnitType   extends Page("Create Unit Type", "fa fa-desktop", "/unittype/create")
-case object ProfileOverview  extends Page("Profile Overview", "fa fa-desktop", "/profile/overview")
-case object ProfileDetails   extends Page("Profile Details", "fa fa-desktop", "/profile/details")
-case object CreateProfile    extends Page("Create Profile", "fa fa-desktop", "/profile/create")
-case object UnitOverview     extends Page("Unit Search", "fa fa-desktop", "/unit/overview")
-case object UnitDetails      extends Page("Unit Details", "fa fa-desktop", "/unit/details")
-case object CreateUnit       extends Page("Create Unit", "fa fa-desktop", "/unit/create")
-case object GroupOverview    extends Page("Group Overview", "fa fa-desktop", "/group/overview")
-case object CreateGroup      extends Page("Create Group", "fa fa-desktop", "/group/create")
-case object JobOverview      extends Page("Job Overview", "fa fa-desktop", "/job/overview")
-case object CreateJob        extends Page("Create Job", "fa fa-desktop", "/job/create")
+case object Dashboard        extends Page("Dashboard", "/")
+case object UnitTypeOverview extends Page("Unit Type Overview", "/unittype/overview")
+case object UnitTypeDetails  extends Page("Unit Type Details", "/unittype/details", false)
+case object CreateUnitType   extends Page("Create Unit Type", "/unittype/create")
+case object ProfileOverview  extends Page("Profile Overview", "/profile/overview")
+case object ProfileDetails   extends Page("Profile Details", "/profile/details", false)
+case object CreateProfile    extends Page("Create Profile", "/profile/create")
+case object UnitOverview     extends Page("Unit Search", "/unit/overview")
+case object UnitDetails      extends Page("Unit Details", "/unit/details", false)
+case object CreateUnit       extends Page("Create Unit", "/unit/create")
+case object GroupOverview    extends Page("Group Overview", "/group/overview")
+case object CreateGroup      extends Page("Create Group", "/group/create")
+case object JobOverview      extends Page("Job Overview", "/job/overview")
+case object CreateJob        extends Page("Create Job", "/job/create")
