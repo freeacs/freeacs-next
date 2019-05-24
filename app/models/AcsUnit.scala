3b8a1ca021f8ee1ddb3c58bd83703ec5b33625d1
Jarl Andre Hubenthal <jarlah@users.noreply.github.com>
2019-05-24 22:21:11 +0200

    reuse constants, dont repeat myself

--- app/models/AcsUnit.scala
+++ app/models/AcsUnit.scala
@@ -1,18 +1,19 @@
 package models
 
 case class AcsUnit(unitId: String, profile: AcsProfile, params: Seq[AcsUnitParameter] = Seq.empty) {
+  import SessionData._
   lazy val unitTypeParams: Seq[AcsUnitTypeParameter] = profile.unitType.params
   lazy val hasConnectionRequestUrl: Boolean =
     params.exists(
-      p => p.unitTypeParamName.endsWith("ConnectionRequestURL") && p.value.exists(!_.isEmpty)
+      p => p.unitTypeParamName.endsWith(connectionRequestUrlSuffix) && p.value.exists(!_.isEmpty)
     )
   lazy val hasConnectionRequestUsername: Boolean =
     params.exists(
-      p => p.unitTypeParamName.endsWith("ConnectionRequestUsername") && p.value.exists(!_.isEmpty)
+      p => p.unitTypeParamName.endsWith(connectionRequestUsernameSuffix) && p.value.exists(!_.isEmpty)
     )
   lazy val hasConnectionRequestPassword: Boolean =
     params.exists(
-      p => p.unitTypeParamName.endsWith("ConnectionRequestPassword") && p.value.exists(!_.isEmpty)
+      p => p.unitTypeParamName.endsWith(connectionRequestPasswordSuffix) && p.value.exists(!_.isEmpty)
     )
   lazy val canKick: Boolean =
     hasConnectionRequestUrl && hasConnectionRequestUsername && hasConnectionRequestPassword
