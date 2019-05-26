4690803bb146f4b61454d21747957ccaac985f42
Jarl Andre Hubenthal <jarlah@users.noreply.github.com>
2019-05-26 14:44:05 +0200

    simplify test

--- test/ApplicationTest.scala
+++ test/ApplicationTest.scala
@@ -2,13 +2,14 @@ import org.scalatestplus.play.PlaySpec
 import org.scalatestplus.play.guice.GuiceOneServerPerTest
 import play.api.{Application, Configuration, Mode}
 import play.api.inject.guice.GuiceApplicationBuilder
-import play.api.libs.ws.{DefaultWSCookie, WSClient}
+import play.api.libs.ws.{DefaultWSCookie, WSClient, WSResponse}
 import services.UnitService
 
-import scala.concurrent.Await
+import scala.concurrent.{Await, Future}
 import scala.concurrent.duration._
 import scala.xml.Utility.trim
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.xml.Elem
 
 class ApplicationTest extends PlaySpec with GuiceOneServerPerTest {
 
@@ -26,27 +27,47 @@ class ApplicationTest extends PlaySpec with GuiceOneServerPerTest {
       .in(Mode.Test)
       .build()
 
-  "the tr069 server creates a unit and completes a normal non-discovery mode provisioning cycle" in {
-    val wsClient = app.injector.instanceOf[WSClient]
-    val baseUrl  = s"http://localhost:$port/tr069"
-    var response = Await.result(wsClient.url(baseUrl).post(inform), Duration.Inf)
-
+  "the tr069 server can provision a unit" in new StatefulTr069Conversation {
+    // 1. IN
+    var response = post(baseUrl, Some(informRequest))
     response.status mustBe 200
-    trim(response.xml) mustBe trim(expectedInformResponse)
-
+    trim(response.xml) mustBe trim(informResponse)
     val unitService = app.injector.instanceOf[UnitService]
     unitService.find("000000-FakeProductClass-FakeSerialNumber") must not be None
 
-    val session = response.header("Set-Cookie").get.stripPrefix("SESSION=").split(";")(0)
-    val cookie  = DefaultWSCookie("SESSION", session)
-    response = Await.result(wsClient.url(baseUrl).withCookies(cookie).post(""), Duration.Inf)
-
+    // 2. EM
+    response = post(baseUrl, None)
     response.status mustBe 200
-    trim(response.xml) mustBe trim(expectedEmptyResponse)
+    trim(response.xml) mustBe trim(getParameterValuesRequest)
+
+    // 3. TODO
   }
 
-  val expectedEmptyResponse =
-    <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:soapenc="http://schemas.xmlsoap.org/soap/encoding/" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:cwmp="urn:dslforum-org:cwmp-1-0">
+  trait StatefulTr069Conversation {
+    val baseUrl: String                  = s"http://localhost:$port/tr069"
+    var session: Option[DefaultWSCookie] = None
+
+    def post(url: String, body: Option[Elem]): WSResponse = {
+      val client  = app.injector.instanceOf[WSClient]
+      val request = session.map(client.url(url).withCookies(_)).getOrElse(client.url(url))
+      Await.result(
+        body.map(request.post(_)).getOrElse(request.post("")).map { response =>
+          session = response.header("Set-Cookie").map { header =>
+            DefaultWSCookie("SESSION", header.stripPrefix("SESSION=").split(";")(0))
+          }
+          response
+        },
+        Duration.Inf
+      )
+    }
+  }
+
+  val getParameterValuesRequest =
+    <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"
+                      xmlns:soapenc="http://schemas.xmlsoap.org/soap/encoding/"
+                      xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+                      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                      xmlns:cwmp="urn:dslforum-org:cwmp-1-0">
       <soapenv:Header>
         <cwmp:ID soapenv:mustUnderstand="1">1</cwmp:ID>
       </soapenv:Header>
@@ -59,12 +80,12 @@ class ApplicationTest extends PlaySpec with GuiceOneServerPerTest {
       </soapenv:Body>
     </soapenv:Envelope>
 
-  val expectedInformResponse =
+  val informResponse =
     <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"
-                                                   xmlns:soapenc="http://schemas.xmlsoap.org/soap/encoding/"
-                                                   xmlns:xsd="http://www.w3.org/2001/XMLSchema"
-                                                   xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-                                                   xmlns:cwmp={s"urn:dslforum-org:cwmp-1-2"}>
+                      xmlns:soapenc="http://schemas.xmlsoap.org/soap/encoding/"
+                      xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+                      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                      xmlns:cwmp={s"urn:dslforum-org:cwmp-1-2"}>
       <soapenv:Header>
         <cwmp:ID soapenv:mustUnderstand="1">1</cwmp:ID>
       </soapenv:Header>
@@ -75,11 +96,11 @@ class ApplicationTest extends PlaySpec with GuiceOneServerPerTest {
       </soapenv:Body>
     </soapenv:Envelope>
 
-  val inform =
+  val informRequest =
     <SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/"
-                                  xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/"
-                                  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema"
-                                  xmlns:cwmp="urn:dslforum-org:cwmp-1-2">
+                       xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/"
+                       xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+                       xmlns:cwmp="urn:dslforum-org:cwmp-1-2">
       <SOAP-ENV:Header>
         <cwmp:ID SOAP-ENV:mustUnderstand="1">1</cwmp:ID>
       </SOAP-ENV:Header>
