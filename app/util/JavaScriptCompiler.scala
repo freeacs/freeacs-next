4afa076d43a32607e798651611a36811548a55e4
Jarl André Hübenthal <jarlah@users.noreply.github.com>
2019-05-29 19:16:58 +0200

    remove scala compiler, improving nashorn compiler, adding work sheet

--- app/util/JavaScriptCompiler.scala
+++ app/util/JavaScriptCompiler.scala
@@ -1,16 +1,23 @@
 package util
-import javax.script.ScriptEngineManager
 import javax.script.Invocable
+import jdk.nashorn.api.scripting.NashornScriptEngineFactory
 
 object JavaScriptCompiler {
-  def compile[A](code: String): Map[String, Any] => A = {
-    val factory = new ScriptEngineManager
-    val engine  = factory.getEngineByName("JavaScript")
+  def compile[A](code: String): java.util.Map[String, Any] => A = {
+    val engine = new NashornScriptEngineFactory().getScriptEngine("--no-java")
     val wrapper =
       s"""
-        |function wrapper(context) {
-        |  $code
-        |}
+         |var quit = function(){ throw 'Unsupported operation: quit'; }
+         |var exit = function(){ throw 'Unsupported operation: exit'; }
+         |var load = function(){ throw 'Unsupported operation: load'; }
+         |var print = function(){ throw 'Unsupported operation: print'; }
+         |var echo = function(){ throw 'Unsupported operation: echo'; }
+         |var readLine = function(){ throw 'Unsupported operation: readLine'; }
+         |var readFully = function(){ throw 'Unsupported operation: readFully'; }
+         |var loadWithNewGlobal = function(){ throw 'Unsupported operation: loadWithNewGlobal'; }
+         |function wrapper(context) {
+         |  $code
+         |}
       """.stripMargin
     engine.eval(wrapper)
     engine.asInstanceOf[Invocable].invokeFunction("wrapper", _).asInstanceOf[A]
