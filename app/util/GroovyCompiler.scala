2c794daa6f6597aacc6ece8a221676fd47c47177
Jarl Andre Hubenthal <jarlah@users.noreply.github.com>
2019-06-03 18:19:30 +0200

    improving groovy compiler example, hiding implementation

--- app/util/GroovyCompiler.scala
+++ app/util/GroovyCompiler.scala
@@ -5,38 +5,44 @@ import org.codehaus.groovy.control.CompilerConfiguration
 import org.kohsuke.groovy.sandbox.{GroovyValueFilter, SandboxTransformer}
 
 object GroovyCompiler extends GroovyValueFilter with App {
-  val ALLOWED_TYPES: Set[Class[_]] = Set(
-    classOf[GroovyContext],
-    classOf[java.lang.String],
-    classOf[java.lang.Integer],
-    classOf[java.lang.Boolean],
-    classOf[java.lang.Double],
-    classOf[java.math.BigDecimal],
-    classOf[java.lang.CharSequence],
-    classOf[java.lang.Float],
-    classOf[java.lang.Short],
-    classOf[java.lang.Byte],
-    classOf[java.lang.Long]
-  )
-
-  override def filter(obj: Object): Object = {
-    if (obj == null || ALLOWED_TYPES.contains(obj.getClass))
-      return obj
-    if (obj.isInstanceOf[Script] || obj.isInstanceOf[Closure[_]])
-      return obj // access to properties of compiled groovy script
-    throw new SecurityException("Oops, unexpected type: " + obj.getClass)
+  private class TypeFilter extends GroovyValueFilter {
+    val ALLOWED_TYPES: Set[Class[_]] = Set(
+      classOf[GroovyContext],
+      classOf[java.lang.String],
+      classOf[java.lang.Integer],
+      classOf[java.lang.Boolean],
+      classOf[java.lang.Double],
+      classOf[java.math.BigDecimal],
+      classOf[java.lang.CharSequence],
+      classOf[java.lang.Float],
+      classOf[java.lang.Short],
+      classOf[java.lang.Byte],
+      classOf[java.lang.Long]
+    )
+    override def filter(obj: Object): Object = {
+      if (obj == null || ALLOWED_TYPES.contains(obj.getClass))
+        return obj
+      if (obj.isInstanceOf[Script] || obj.isInstanceOf[Closure[_]])
+        return obj // access to properties of compiled groovy script
+      throw new SecurityException("Oops, unexpected type: " + obj.getClass)
+    }
   }
 
-  val cc = new CompilerConfiguration()
+  new TypeFilter().register()
+
+  private val cc = new CompilerConfiguration()
   cc.addCompilationCustomizers(new SandboxTransformer())
-  val binding = new Binding()
+  private val binding = new Binding()
   binding.setProperty("context", new GroovyContext())
-  val sh = new GroovyShell(binding, cc)
+  private val sh = new GroovyShell(binding, cc)
 
-  register()
+  def execute(script: String): String = sh.evaluate(script).asInstanceOf[String]
 
-  sh.evaluate(""" context.exit(); """)  // Success because its GroovyContext
-  sh.evaluate(""" System.exit(-1); """) // fails because its Class
-  sh.evaluate(""" println "Hei"; """)   // Success because its String
-  sh.evaluate(""" println 0.23 """)     // fails because its BigDecimal
+  println(execute(""" 
+      |def sayHello(name) {
+      | "Hello, " + name
+      |}
+      |
+      |sayHello("World");
+      |""".stripMargin))
 }
--- app/util/GroovyContext.java
+++ app/util/GroovyContext.java
@@ -1,7 +1,5 @@
 package util;
 
+// TODO to be made service that exposes methods to fetch stuff from the database etc
 public class GroovyContext {
-    public void exit() {
-        System.exit(-1);
-    }
 }
